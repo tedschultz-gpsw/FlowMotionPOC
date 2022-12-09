072c0f049235c332fe6568a44f53d2d77bf2e8b0
Ted Schultz <tedschultz@gopro.com>
2022-12-09 15:57:28 -0800

    Reset project

--- FlowMotionPOC-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-// MARK: Use this to enable C and Obj-C files
-#import "GPSphericalMetadataProvider.h"
-#import ""
--- FlowMotionPOC/FlowMotionPOC/ViewController.swift
+++ FlowMotionPOC/FlowMotionPOC/ViewController.swift
@@ -8,10 +8,25 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    func getVideo() {
+        let panel = NSOpenPanel()
+        panel.runModal()
+        guard let url = panel.urls.first else { return }
+        let data = try? Data(contentsOf: url)
+    }
+    
+    func getCSVFile() {
+        let panel = NSOpenPanel()
+        panel.runModal()
+        guard let url = panel.urls.first else { return }
+        let data = try? Data(contentsOf: url)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getVideo()
+        getCSVFile()
         // Do any additional setup after loading the view.
     }
 
