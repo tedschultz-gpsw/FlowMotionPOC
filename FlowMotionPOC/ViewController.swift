a2e4ff46cdd20de9b2674a8eba50304aa1994c71
Ted Schultz <tedschultz@gopro.com>
2022-12-09 10:59:57 -0800

    Initial Commit

---
+++ FlowMotionPOC/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FlowMotionPOC
+//
+//  Created by tedschultz on 12/9/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
